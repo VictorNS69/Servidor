5e617aad9989ffd36cc1e0085a3394ad6661ec31
Victor Nieves Sanchez <vnievess@gmail.com>
2018-12-02 18:01:28 +0100

    New lines. NOTA = 0

--- servidor.c
+++ servidor.c
@@ -1,21 +1,21 @@
 /* Autor: Victor Nieves Sanchez
- * servidor: servidor pseudo FTP.
- *
- * Copyright (c) 2013,2018 DATSI. Francisco Rosales <frosal@fi.upm.es>
- * Todos los derechos reservados.
- *
- * Publicado bajo Licencia de Proyecto Educativo Práctico
- * <http://laurel.datsi.fi.upm.es/~ssoo/LICENCIA/LPEP>
- *
- * Queda prohibida la difusión total o parcial por cualquier
- * medio del material entregado al alumno para la realización
- * de este proyecto o de cualquier material derivado de este,
- * incluyendo la solución particular que desarrolle el alumno.
- */
+* servidor: servidor pseudo FTP.
+*
+* Copyright (c) 2013,2018 DATSI. Francisco Rosales <frosal@fi.upm.es>
+* Todos los derechos reservados.
+*
+* Publicado bajo Licencia de Proyecto Educativo Práctico
+* <http://laurel.datsi.fi.upm.es/~ssoo/LICENCIA/LPEP>
+*
+* Queda prohibida la difusión total o parcial por cualquier
+* medio del material entregado al alumno para la realización
+* de este proyecto o de cualquier material derivado de este,
+* incluyendo la solución particular que desarrolle el alumno.
+*/
 
 //includes enunciado
 #include <sys/types.h>
-#include <sys/stat.h>
+//#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -31,105 +31,148 @@ void Escribir_Puerto (int puerto);
 /* FUNCION MAIN DEL PROGRAMA SERVIDOR */
 int main(int argc,char* argv[]){
 
-   /* El esqueleto de la funcion principal muestra, de forma esquematica la secuencia
-   de operaciones y la correspondiente traza que se sugiere */
-   int socketTCP, socketUDP;
-   int puertoTCP;
-   struct sockaddr_in s_ain;
-   socklen_t size = sizeof(s_ain);
-   /* Creacion del socket UDP */
-   fprintf(stderr,"SERVIDOR: Creacion del socket UDP: ");
-   if ((socketUDP = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-       fprintf(stderr, "ERROR\n");
-       exit(1);
-   }
-   fprintf(stderr, "OK\n");
+  /* El esqueleto de la funcion principal muestra, de forma esquematica la secuencia
+  de operaciones y la correspondiente traza que se sugiere */
+  int sTCP, sUDP, pTCP;
+  struct sockaddr_in sin;
+  socklen_t size = sizeof(sin);
+  int msize, file, cs;
+  size_t len;
+  ssize_t result;
+  UDP_Msg msg;
+  char buffer[1024];
 
-   /* Asignacion de la direccion local (del servidor) Puerto UDP*/
-   bzero((char *) &s_ain, sizeof(s_ain));
-   s_ain.sin_family = AF_INET;
-   s_ain.sin_addr.s_addr = inet_addr(HOST_SERVIDOR);
-   s_ain.sin_port = htons(0);
-   fprintf(stderr, "SERVIDOR: Asignacion del puerto servidor: ");
-   if (bind(socketUDP, (struct sockaddr *) &s_ain, sizeof(s_ain)) < 0) {
-       fprintf(stderr, "ERROR\n");
-       close(socketUDP);
-       exit(1);
-   }
-   fprintf(stderr, "OK\n");
+  /* Creacion del socket UDP */
+  fprintf(stdout,"SERVIDOR: Creacion del socket UDP: ");
+  if ((sUDP = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    fprintf(stdout, "ERROR\n");
+    exit(1);
+  }
+  fprintf(stdout, "OK\n");
 
-   /* Escribimos el puerto de servicio */
-   bzero((char *) &s_ain, sizeof(struct sockaddr_in)); /* Pone a 0*/
-   getsockname(socketUDP, (struct sockaddr *) &s_ain, &size);
-   Escribir_Puerto(ntohs((uint16_t) (int) s_ain.sin_port) /* Numero de puerto asignado */);
+  /* Asignacion de la direccion local (del servidor) Puerto UDP*/
+  bzero((char *) &sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = inet_addr(HOST_SERVIDOR);
+  sin.sin_port = htons(0);
+  fprintf(stdout, "SERVIDOR: Asignacion del puerto servidor: ");
+  if (bind(sUDP, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+    fprintf(stdout, "ERROR\n");
+    close(sUDP);
+    exit(1);
+  }
+  fprintf(stdout, "OK\n");
 
-   /* Creacion del socket TCP de servicio */
-   fprintf(stderr,"SERVIDOR: Creacion del socket TCP: ");
-   if ((socketTCP = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-       fprintf(stderr, "ERROR\n");
-       exit(1);
-   }
-   fprintf(stderr, "OK\n");
+  /* Escribimos el puerto de servicio */
+  bzero((char *) &sin, sizeof(struct sockaddr_in)); /* Pone a 0*/
+  getsockname(sUDP, (struct sockaddr *) &sin, &size);
+  Escribir_Puerto(ntohs((uint16_t) (int) sin.sin_port) /* Numero de puerto asignado */);
 
-   /* Asignacion de la direccion local (del servidor) Puerto TCP*/
-   s_ain.sin_family = AF_INET;
-   s_ain.sin_addr.s_addr = inet_addr(HOST_SERVIDOR);
-   s_ain.sin_port = htons(0);
-   fprintf(stderr,"SERVIDOR: Asignacion del puerto servidor: ");
-   if (bind(socketTCP, (struct sockaddr *) &s_ain, sizeof(s_ain)) < 0) {
-       fprintf(stderr, "ERROR\n");
-       close(socketTCP);
-       exit(1);
-   }
-   fprintf(stderr, "OK\n");
-   bzero((char *) &s_ain, sizeof(struct sockaddr_in)); /* Pone a 0*/
-   getsockname(socketTCP, (struct sockaddr *) &s_ain, &size);
-   puertoTCP = htons(s_ain.sin_port);
+  /* Creacion del socket TCP de servicio */
+  fprintf(stdout,"SERVIDOR: Creacion del socket TCP: ");
+  if ((sTCP = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    fprintf(stdout, "ERROR\n");
+    exit(1);
+  }
+  fprintf(stdout, "OK\n");
 
-   /* Aceptamos conexiones por el socket */
-   fprintf(stderr,"SERVIDOR: Aceptacion de peticiones: ");
-   if (listen(socketTCP, 10) < 0) {
-       fprintf(stderr, "ERROR\n");
-       fprintf(stderr, "SERVIDOR: Puerto TCP reservado: ERROR\n");
-       exit(1);
-   }
-   fprintf(stderr, "OK\n");
+  /* Asignacion de la direccion local (del servidor) Puerto TCP*/
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = inet_addr(HOST_SERVIDOR);
+  sin.sin_port = htons(0);
+  fprintf(stdout,"SERVIDOR: Asignacion del puerto servidor: ");
+  if (bind(sTCP, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
+    fprintf(stdout, "ERROR\n");
+    close(sTCP);
+    exit(1);
+  }
+  fprintf(stdout, "OK\n");
+  bzero((char *) &sin, sizeof(struct sockaddr_in)); /* Pone a 0*/
+  getsockname(sTCP, (struct sockaddr *) &sin, &size);
+  pTCP = htons(sin.sin_port);
 
-   /* Puerto TCP ya disponible */
-   fprintf(stderr,"SERVIDOR: Puerto TCP reservado: OK\n");
-   exit(0);
-   while(1 /* Bucle de procesar peticiones */)
-   {
-      fprintf(stderr,"SERVIDOR: Esperando mensaje.\n");
+  /* Aceptamos conexiones por el socket */
+  fprintf(stdout,"SERVIDOR: Aceptacion de peticiones: ");
+  if (listen(sTCP, 10) < 0) {
+    fprintf(stdout, "ERROR\n");
+    fprintf(stdout, "SERVIDOR: Puerto TCP reservado: ERROR\n");
+    exit(1);
+  }
+  fprintf(stdout, "OK\n");
 
-      /* Recibo mensaje */
-      fprintf(stderr,"SERVIDOR: Mensaje del cliente: OK|ERROR\n");
+  /* Puerto TCP ya disponible */
+  fprintf(stdout,"SERVIDOR: Puerto TCP reservado: OK\n");
+  while(1 /* Bucle de procesar peticiones */){
+    fprintf(stdout,"SERVIDOR: Esperando mensaje.\n");
+    bzero((char *) &sin, sizeof(struct sockaddr_in));
+    size = sizeof(sin);
+    bzero(&msg, sizeof(UDP_Msg));
+    msize = sizeof(UDP_Msg);
 
-      if(1/* Mensaje QUIT*/)
-           fprintf(stderr,"SERVIDOR: QUIT\n");
+    /* Recibo msg */
+    fprintf(stdout, "SERVIDOR: mensaje del cliente: ");
+    if (recvfrom(sUDP, (char *) &msg, (size_t) msize, 0, (struct sockaddr *) &sin, &size) < 0) {
+      fprintf(stdout, "ERROR\n");
+      break;
+    }
+    fprintf(stdout, "OK\n");
+    if(ntohl(msg.op) == QUIT/* msg QUIT*/){
+      fprintf(stdout,"SERVIDOR: QUIT\n");
+      fprintf(stderr,"SERVIDOR: Enviando del resultado [OK]: ");
+      result = sendto(sUDP, (char *) &msg, (size_t) msize, 0, (struct sockaddr *) &sin, size);
+      if (result < 0)
+        fprintf(stdout, "ERROR\n");
       else
-      {
-           fprintf(stderr,"SERVIDOR: REQUEST(%s,%s)\n", "local","remoto");
+        fprintf(stdout, "OK\n");
+      break;
+    }
+    else{
+      fprintf(stdout,"SERVIDOR: REQUEST(%s,%s)\n", msg.local, msg.remoto);
 
-           /* Envio del resultado */
-           fprintf(stderr,"SERVIDOR: Enviando del resultado [OK|ERROR]: OK|ERROR\n");
-           /* Esperamos la llegada de una conexion */
-           fprintf(stderr,"SERVIDOR: Llegada de un mensaje: OK|ERROR\n");
+      /* Envio del resultado */
+      file = open(msg.remoto, O_RDONLY);
+      if (file < 0) {
+        fprintf(stdout,"SERVIDOR: Enviando del resultado [ERROR]: ");
+        msg.op = htonl(ERROR);
+      } else {
+        fprintf(stdout,"SERVIDOR: Enviando del resultado [OK]: ");
+        msg.op = htonl(OK);
+        msg.puerto = htons((uint16_t) pTCP);
       }
-   }
+      result = sendto(sUDP, (char *) &msg, (size_t) msize, 0, (struct sockaddr *) &sin, size);
+      if (result < 0)
+        fprintf(stdout, "ERROR\n");
+      else
+        fprintf(stdout, "OK\n");
 
-   fprintf(stderr,"SERVIDOR: Finalizado\n");
-   exit(0);
+      /* Esperamos la llegada de una conexion */
+      fprintf(stdout,"SERVIDOR: Llegada de un mensaje: ");
+      if (msg.op == htonl(OK)) {
+        cs = accept(sTCP, (struct sockaddr *) &sin, &stdout);// STDOUT -> NULL?
+        if (cs < 0)
+          fprintf(stdout, "ERROR\n");
+        else
+          fprintf(stdout, "OK\n");
+        while ((len = (size_t) read(file, buffer, 1024)) > 0)
+          write(cs, buffer, len);
+        close(cs);
+      }
+      close(file);
+    }
+  }
+  //Cerramos todo lo que quede por cerrar
+  close(sTCP);
+  close(sUDP);
+  fprintf(stdout,"SERVIDOR: Finalizado\n");
+  exit(0);
 }
 
-/* Funcion auxiliar que escribe un numero de puerto en el fichero */
-void Escribir_Puerto (int puerto)
-{
-   int fd;
-   if((fd=creat(FICHERO_PUERTO,0660))>=0)
-   {
-      write(fd,&puerto,sizeof(int));
-      close(fd);
-      fprintf(stderr,"SERVIDOR: Puerto guardado en fichero %s: OK\n",FICHERO_PUERTO);
-   }
+/* Funcion auxiliar que escribe un numero de puerto en el file */
+void Escribir_Puerto (int puerto){
+  int fd;
+  if((fd=creat(FICHERO_PUERTO,0660))>=0){
+    write(fd,&puerto,sizeof(int));
+    close(fd);
+    fprintf(stdout,"SERVIDOR: Puerto guardado en fichero %s: OK\n",FICHERO_PUERTO);
+  }
 }
